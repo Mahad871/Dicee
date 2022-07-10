2352773633ec759c83cfa424ee46eee962b5bf6d
Mahad Saleem <ms.asd871@gmail.com>
2022-07-10 11:17:53 +0500

    dicee push

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:portfolio/main.dart';
 
-import 'package:dice_roller/main.dart';
+// import 'package:dice_roller/.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
