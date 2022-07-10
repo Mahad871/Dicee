0a51d6adb8d7ae908c058f90ea26c6e907021a3c
Mahad Saleem <ms.asd871@gmail.com>
2022-07-10 11:05:25 +0500

    dicee app complete

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, avoid_print
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red.shade900,
+        appBar: AppBar(
+          title: Center(
+            child: Text('Dicee'),
+          ),
+          backgroundColor: Colors.red.shade900,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var ldn = 1, rdn = 1;
+  void btnPress() {
+    setState(() {
+      ldn = Random().nextInt(6) + 1;
+      rdn = Random().nextInt(6) + 1;
+    });
+  }
+//git push test 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: TextButton(
+                  onPressed: btnPress,
+                  child: Image.asset('images/dice$ldn.png'),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: TextButton(
+                  onPressed: () {
+                    btnPress();
+                  },
+                  child: Image.asset('images/dice$rdn.png'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
